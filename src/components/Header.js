7e2f46a9c3311f998936d829a427afd55191a53b
Alec Butterfield <alec.butterfield1@gmail.com>
2023-06-28 13:20:24 -0700

    Header skeleton created

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,6 +2,18 @@ import React from "react";
 
 export default function Header() {
     return (
-        <h1>Header</h1>
+        <div className="header-main-container">
+            <div className="header-top-section">
+                <img className="header-personal-img" alt="myImage" />
+                <h1 className="header-personal-name">Alec Butterfield</h1>
+            </div>
+            <div className="header-bottom-section">
+                <a>Home</a>
+                <a>Projects</a>
+                <a>Goals</a>
+                <a>Gizmos</a>
+                <a>Contact</a>
+            </div>
+        </div>
     )
 }
