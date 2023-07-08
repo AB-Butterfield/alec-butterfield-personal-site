ff42db1815f1b1550c3070495b9d1de271eb5156
Alec B <alec.butterfield1@gmail.com>
2023-07-08 00:09:47 -0700

    Footer and Header styles updates

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,19 @@
 import React from "react";
 
 export default function Footer() {
+
+    const contactMeInfo = "Do you need an enthusiastic individual to join your development team? Email me anytime, at email@gmail.com!"
+    const pageInfo = "site by Alec Butterfield - 2023"
     return (
-        <h3>Footer</h3>
+        <div>
+            <div className="footer-main-container">
+                <div className="footer-contact">
+                    <p>{contactMeInfo}</p>
+                </div>
+                <div className="footer-information">
+                    <p>{pageInfo}</p>
+                </div>
+            </div>
+        </div>
     )
 }
