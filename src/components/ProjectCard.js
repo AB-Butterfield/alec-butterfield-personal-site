ca585f5edbf4a304d4302343c2b4b250260965f5
Alec B <alec.butterfield1@gmail.com>
2023-07-08 22:40:11 -0700

    Projects functionality created

--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -1,13 +1,19 @@
 import React from "react";
+import blueprintImg from "../images/blueprint-img.png"
+
 
 export default function ProjectCard(props) {
-    let {id, title, img, dateCreated, description} = props
-    console.log(props)
+    let {id, title, img, website, dateCreated, description} = props
+    let websiteMessage = ""
+    if (website) {
+        websiteMessage = "Click here for website!"
+    }
+    
     return (
         <div className="single-project-container">
-            <div className="single-project-id">{id}</div>
             <div className="single-project-title">{title}</div>
-            <div className="single-project-img">{img}</div>
+            <a className="single-project-website" href={website && {website}}>{websiteMessage}</a>
+            <img className="single-project-img" src={require(`../images/${img}`)} alt="project"></img>
             <div className="single-project-dateCreated">{dateCreated}</div>
             <div className="single-project-description">{description}</div>
         </div>
