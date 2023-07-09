f1491139880524407dba7cc9fc2a9d19285eacfe
Alec B <alec.butterfield1@gmail.com>
2023-07-08 22:57:11 -0700

    Fixed a merge issue

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,18 +1,20 @@
 import React from "react";
+import profilePic from "../images/Alec-Profile-Pic.jpg"
 
 export default function Header() {
+
     return (
         <div className="header-main-container">
             <div className="header-top-section">
-                <img className="header-personal-img" alt="myImage" />
+                <img className="header-personal-img skeleton" src={profilePic} alt="myImage" />
                 <h1 className="header-personal-name">Alec Butterfield</h1>
             </div>
             <div className="header-bottom-section">
-                <a href = "./home">Home</a>
-                <a href="./projects">Projects</a>
-                <a href="./goals">Goals</a>
-                <a href="./gizmos">Gizmos</a>
-                <a href = "./contact">Contact</a>
+                <a className="header-link" href = "./home">Home</a>
+                <a className="header-link" href="./projects">Projects</a>
+                <a className="header-link" href="./goals">Goals</a>
+                <a className="header-link" href="./gizmos">Gizmos</a>
+                <a className="header-link" href = "./contact">Contact</a>
             </div>
         </div>
     )
