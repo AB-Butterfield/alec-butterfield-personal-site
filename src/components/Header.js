1642134f268c6551c7c91ed97f73c5a2c8b0be54
Alec B <alec.butterfield1@gmail.com>
2023-07-08 19:26:01 -0700

    Header styling updated

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,12 @@
 import React from "react";
+import profilePic from "../images/Alec-Profile-Pic.jpg"
 
 export default function Header() {
+    
     return (
         <div className="header-main-container">
             <div className="header-top-section">
-                <img className="header-personal-img" alt="myImage" />
+                <img className="header-personal-img skeleton" src={profilePic} alt="myImage" />
                 <h1 className="header-personal-name">Alec Butterfield</h1>
             </div>
             <div className="header-bottom-section">
