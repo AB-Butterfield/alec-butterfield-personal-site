0f254ff3eea5cf974a34d43b917f5329b4194cbf
Alec Butterfield <alec.butterfield1@gmail.com>
2023-07-12 15:30:24 -0700

    Header, Footer, Home now mobile friendly

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,13 +2,13 @@ import React from "react";
 
 export default function Footer() {
 
-    const contactMeInfo = "Do you need an enthusiastic individual to join your development team? Email me anytime, at alec.butterfield1@gmail.com!"
+    const contactMeInfo = "Looking for an enthusiastic individual to join your team? "
     const pageInfo = "site by Alec Butterfield - 2023"
     return (
         <div>
             <div className="footer-main-container">
                 <div className="footer-contact">
-                    <p>{contactMeInfo}</p>
+                    <p>{contactMeInfo}<br/><a href="./contact">Contact me!</a></p>
                 </div>
                 <div className="footer-information">
                     <p>{pageInfo}</p>
