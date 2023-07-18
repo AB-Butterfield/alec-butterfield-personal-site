ab29da402bd3a95ca53387655f12f9c2efdc4083
Alec Butterfield <alec.butterfield1@gmail.com>
2023-07-17 23:17:28 -0700

    Gizmo carousel started

---
+++ src/db/gizmoData.js
@@ -0,0 +1,19 @@
+export const gizmoData = [
+    {
+    title: "carousel",
+    data: [
+            {
+                id: 1,
+                name: "boba1"
+            },
+            {
+                id: 2,
+                name: "boba2"
+            },
+            {
+                id: 3,
+                name: "boba3"
+            }
+        ]
+    }
+]
--- src/pages/Gizmos.js
+++ src/pages/Gizmos.js
@@ -1,9 +1,52 @@
+import React, { useEffect } from "react"
+import { useState } from "react"
+import CarouselCard from "../components/CarouselCard"
+import { gizmoData } from "../db/gizmoData"
 
 
 export default function Gizmos() {
+    const [carouselIdx, setCarouselIdx] = useState(0)
+    let carouselData = gizmoData[0].data
+    const [carouselArray] = useState(carouselData)
+    // let carouselArray = carouselData.map(item => {
+    //     return (
+    //         <CarouselCard 
+    //         key = {carouselData.id}
+    //         {...item}
+    //         />
+    //     )
+    // })
+    console.log(carouselArray)
+
+    //Carousel
+    useEffect(() => {
+        const lastIdx = (carouselArray.length - 1)
+        if (carouselIdx > lastIdx) {
+            setCarouselIdx(0)
+            console.log('Going to beginning...')
+        }
+        if (carouselIdx < lastIdx) {
+            setCarouselIdx(lastIdx)
+            console.log('Going to end')
+        }
+    }, [carouselIdx, carouselArray  ])
+
+    // useEffect(() => {
+    //     let slider = setInterval(() => {
+    //         setCarouselIdx(carouselIdx + 1)
+    //         console.log('Changing Idx...', carouselIdx)
+    //     }, 5000)
+    //     return () => {
+    //         clearInterval(slider)
+    //     }
+    // }, [])
+
     return (
-        <div className="gizmos-main-container">
-            <div>Coming soon!</div>
+        <div className="gizmos-main-container">\
+            Coming soon!
+            <div className="gizmos-carousel-main-container">
+                {/* {carouselArray[carouselIdx]} */}
+            </div>
         </div>
     )
 }
