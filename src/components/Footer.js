41ad8f29341df7ff33763e862e5e889305ffee6f
Alec Butterfield <alec.butterfield1@gmail.com>
2024-01-11 19:25:51 -0600

    Initial branch, footer commented out for easier viewing

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,14 +6,14 @@ export default function Footer() {
     const pageInfo = "site by Alec Butterfield - 2023"
     return (
         <div>
-            <div className="footer-main-container">
+            {/* <div className="footer-main-container">
                 <div className="footer-contact">
                     <p>{contactMeInfo}<br/><a href="./contact">Contact me!</a></p>
                 </div>
                 <div className="footer-information">
                     <p>{pageInfo}</p>
                 </div>
-            </div>
+            </div> */}
         </div>
     )
 }
